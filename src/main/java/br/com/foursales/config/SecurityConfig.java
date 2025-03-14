685b929846fbd997db79bc89f4783be58ea9a354
Francisco Miguel <franciscomiguel2003@gmail.com>
2025-03-13 21:37:24 -0300

    Ajustes de autenticação

---
+++ src/main/java/br/com/foursales/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package br.com.foursales.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+
+
+}
